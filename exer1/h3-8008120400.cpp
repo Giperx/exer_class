3d6d32c575dfb56f7a785253346f593a03f8e6ac
Giperx <1512739072@qq.com>
2022-05-29 18:31:20 +0800

    readme part some

--- exer1/h3-8008120400.cpp
+++ exer1/h3-8008120400.cpp
@@ -11,35 +11,35 @@ int main(){
     double s = 0;
     cout << "1、圆形\n2、长方形\n3、正方形\n请选择计算图形的形状：";
     cin >> t;
-        switch (t) {
-            case 1: {
-                double r = 0;
-                cout << "输入圆的半径r:";
-                cin >> r;
-                s = PI * r * r;
-                cout << "圆的面积为：" << s << endl;
-                break;
-            }
-            case 2: {
-                int l, w;
-                cout << "输入长方形的长、宽：";
-                cin >> l >> w;
-                s = l * w;
-                cout << "长方形的面积为：" << s << endl;
-                break;
-            }
-            case 3:{
-                int h;
-                cout << "输入正方形的边长：";
-                cin >> h;
-                s = h * h;
-                cout << "正方形的面积为：" << s << endl;
-                break;
-            }
-            default: {
-                cout << "输入错误!" << endl;
-                break;
-            }
+    switch (t) {
+        case 1: {
+            double r = 0;
+            cout << "输入圆的半径r:";
+            cin >> r;
+            s = PI * r * r;
+            cout << "圆的面积为：" << s << endl;
+            break;
+        }
+        case 2: {
+            int l, w;
+            cout << "输入长方形的长、宽：";
+            cin >> l >> w;
+            s = l * w;
+            cout << "长方形的面积为：" << s << endl;
+            break;
+        }
+        case 3:{
+            int h;
+            cout << "输入正方形的边长：";
+            cin >> h;
+            s = h * h;
+            cout << "正方形的面积为：" << s << endl;
+            break;
+        }
+        default: {
+            cout << "输入错误!" << endl;
+            break;
+        }
     }
     return 0;
 }
